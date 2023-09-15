d9258b39e6dea2c61f66299aa0033a3cae3bbb52
samuel <sammykuria300@gmail.com>
2023-09-15 12:21:53 +0300

    Quick commit

---
+++ practice.js
@@ -0,0 +1,13 @@
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase()==True){
+        return "I can't hear you";
+    }
+    elif (string.toUpperCase()==True);
+     return "YES INDEED!";
+    
+    elif (string=="Let's have dinner together!");
+        return "I would love to!";
+    
+   
+  }
